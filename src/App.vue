15f3fdc4df8aa1709ab9548b8db1cdf8927dcd8b
SokunbophaSovann2003 <sokunbophasovann@gmail.com>
2025-04-03 10:38:45 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
